7820ce87d0023ca2afef4df89985d90f4550c36a
ldg981004 <ldg981004@naver.com>
2021-10-14 19:22:21 +0900

    10/14(동길)

--- app/src/main/java/com/example/toothscheduleproject/Schedule.java
+++ app/src/main/java/com/example/toothscheduleproject/Schedule.java
@@ -32,4 +32,6 @@ public class Schedule extends Activity {
         });
     }
 
+
+
 }
--- app/src/main/java/com/example/toothscheduleproject/SurveyResult.java
+++ app/src/main/java/com/example/toothscheduleproject/SurveyResult.java
@@ -1,4 +1,12 @@
 package com.example.toothscheduleproject;
 
-public class SurveyResult {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SurveyResult extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.survey_result);
+    }
 }
