f31cad43f2fa5fa9bc21e72c365540058468cd12
ldg981004 <ldg981004@naver.com>
2021-10-14 14:52:29 +0900

    Merge branch 'master' of https://github.com/YunSuJeong/ToothScheduleProject
     Conflicts:
    	.idea/misc.xml
    	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/toothscheduleproject/Schedule.java
+++ app/src/main/java/com/example/toothscheduleproject/Schedule.java
@@ -1,4 +1,35 @@
 package com.example.toothscheduleproject;
 
-public class Schedule {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class Schedule extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.schedule);
+
+        ImageButton imbtnBack = (ImageButton) findViewById(R.id.imbtnBack);
+        imbtnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnMorning  = (Button) findViewById(R.id.btnMorning);
+        btnMorning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ScheduleDetail.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/toothscheduleproject/ScheduleDetail.java
+++ app/src/main/java/com/example/toothscheduleproject/ScheduleDetail.java
@@ -1,4 +1,26 @@
 package com.example.toothscheduleproject;
 
-public class ScheduleDetail {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class ScheduleDetail extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.schedule_detail);
+
+        ImageButton imbtnBack = (ImageButton) findViewById(R.id.imbtnBack);
+        imbtnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Schedule.class);
+                startActivity(intent);
+            }
+        });
+
+    }
 }
