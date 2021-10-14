dec23323ae0b4bf108f19533492caa6e00e7cde8
YunSuJeong <jennisu1@naver.com>
2021-10-14 20:15:35 +0900

    10.14 (수정)

--- app/src/main/java/com/example/toothscheduleproject/Notification.java
+++ app/src/main/java/com/example/toothscheduleproject/Notification.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.Switch;
 
 public class Notification extends Activity {
     @Override
@@ -13,11 +15,31 @@ public class Notification extends Activity {
         setContentView(R.layout.notification);
 
         ImageButton ibtnBack = (ImageButton)findViewById(R.id.ibtnBack);
+        Switch swAlarm = (Switch)findViewById(R.id.swAlarm);
+        ImageButton ibtnAddAlarm = (ImageButton)findViewById(R.id.ibtnAddAlarm);
 
         ibtnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        swAlarm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    ibtnAddAlarm.setVisibility(View.VISIBLE);
+                    ibtnAddAlarm.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(getApplicationContext(), NotificationDetail.class);
+                            startActivity(intent);
+                        }
+                    });
+                } else {
+                    ibtnAddAlarm.setVisibility(View.INVISIBLE);
+                }
             }
         });
 
--- app/src/main/java/com/example/toothscheduleproject/NotificationDetail.java
+++ app/src/main/java/com/example/toothscheduleproject/NotificationDetail.java
@@ -1,4 +1,24 @@
 package com.example.toothscheduleproject;
 
-public class NotificationDetail {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class NotificationDetail extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification_detail);
+
+        ImageButton ibtnBack = (ImageButton) findViewById(R.id.ibtnBack);
+
+        ibtnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
