f50d00ba3b3b02ce7a41386ed8ef873cb7304638
ldg981004 <ldg981004@naver.com>
2021-10-21 14:37:22 +0900

    비밀번호 스패이스 텍스트 지움(동길)

--- app/src/main/java/com/example/toothscheduleproject/SurveyResult.java
+++ app/src/main/java/com/example/toothscheduleproject/SurveyResult.java
@@ -13,13 +13,13 @@ public class SurveyResult extends Activity {
         setContentView(R.layout.survey_result);
 
         Button btnstart = (Button) findViewById(R.id.btnstart);
+
         btnstart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
-
         });
     }
 
