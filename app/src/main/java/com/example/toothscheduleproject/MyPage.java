35083481edbd465509c6a690940a42b4904d42cd
YunSuJeong <jennisu1@naver.com>
2021-10-21 15:46:48 +0900

    10.21

--- app/src/main/java/com/example/toothscheduleproject/MyPage.java
+++ app/src/main/java/com/example/toothscheduleproject/MyPage.java
@@ -6,11 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MyPage extends Activity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private DatabaseReference mDatabaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +31,34 @@ public class MyPage extends Activity {
         ImageButton ibtnBack = (ImageButton) findViewById(R.id.ibtnBack);
         Button btnGraph = (Button) findViewById(R.id.btnGraph);
         Button btnLogout = (Button) findViewById(R.id.btnLogout);
+        TextView tvID = findViewById(R.id.tvID);
+        TextView tvUserName = findViewById(R.id.tvUserName);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("ToothSchedule");
+
+        // 로그인 되어있는 사용자 정보를 보여준다.
+        mDatabaseRef.child("UserInfo").orderByChild("idToken").equalTo(mFirebaseAuth.getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserInfo userInfo = null;
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    userInfo = dataSnapshot.getValue(UserInfo.class);
+                }
+                if ( userInfo != null ) {
+                    String userId = userInfo.getEmailId();
+                    String userName = userInfo.getUserName();
+
+                    tvID.setText(userId);
+                    tvUserName.setText(userName);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
         // 뒤로가기 버튼 눌렀을 때
