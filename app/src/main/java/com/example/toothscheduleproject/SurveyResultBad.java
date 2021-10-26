6ff30da3251955fcd3e188d2912d525f1d0b45ee
ldg981004 <ldg981004@naver.com>
2021-10-27 00:35:39 +0900

    구글 지도 나옴

--- app/src/main/java/com/example/toothscheduleproject/SurveyResultBad.java
+++ app/src/main/java/com/example/toothscheduleproject/SurveyResultBad.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SurveyResultBad extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SurveyResultBad extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
