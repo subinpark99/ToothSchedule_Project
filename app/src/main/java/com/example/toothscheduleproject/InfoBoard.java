275f346a932f92794f5471b6f144059712aebccd
subinpark99 <psb8909@naver.com>
2021-10-26 19:32:23 +0900

    정보게시판 자바

--- app/src/main/java/com/example/toothscheduleproject/InfoBoard.java
+++ app/src/main/java/com/example/toothscheduleproject/InfoBoard.java
@@ -1,35 +1,61 @@
 package com.example.toothscheduleproject;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-public class InfoBoard extends Activity{
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import me.relex.circleindicator.CircleIndicator3;
+
+public class InfoBoard extends AppCompatActivity {
+
+    private ViewPager2 mPager;
+    private FragmentStateAdapter pagerAdapter;
+    private int num_page = 6;
+    private CircleIndicator3 mIndicator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.infoboard);
+        setContentView(R.layout.activity_main);
+
+        /**
+         * 가로 슬라이드 뷰 Fragment
+         */
+
+        //ViewPager2
+        mPager = findViewById(R.id.pager);
+        //Adapter
+        pagerAdapter = new MyAdapter(this, num_page);
+        mPager.setAdapter(pagerAdapter);
+        //Indicator
+        mIndicator = findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
+        mIndicator.createIndicators(num_page,0);
+        //ViewPager Setting
+        mPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
 
 
-        Button btnHome = findViewById(R.id.btnHome);
-        Button btnInfo = findViewById(R.id.btnInfo);
 
-        // 홈 버튼 눌렀을 때 메인페이지로 이동
-        btnHome.setOnClickListener(new View.OnClickListener() {
+        mPager.setCurrentItem(1000); //시작 지점
+        mPager.setOffscreenPageLimit(6); //최대 이미지 수
+
+        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                if (positionOffsetPixels == 0) {
+                    mPager.setCurrentItem(position);
+                }
             }
-        });
 
-        btnInfo.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                mIndicator.animatePageSelected(position%num_page);
             }
         });
     }
+
 }
